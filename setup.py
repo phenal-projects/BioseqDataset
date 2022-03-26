70c59e332dea2abbcf3f356646f4af5dfed74ff2
Ilya Bushmakin <phenal.projects@protonmail.com>
2022-03-26 16:41:21 +0300

    Added setup, cleaned the code

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("./requirements.txt") as fin:
+    requirements = [line.strip() for line in fin]
+
+setup(
+    name="bioseq_dataset",
+    version="2022.03",
+    description="A database for biological sequences",
+    url="https://github.com/phenal-projects/BioseqDataset",
+    author='Ilya Bushmakin',
+    author_email='isnicsi@gmail.com',
+    license="MIT",
+    packages=["bioseq_dataset"],
+    install_requires=requirements,
+)
